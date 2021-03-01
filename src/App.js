e995d77f130073a10a7efe3f6fda0f1070ece3a9
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-03-01 15:05:56 +0200

    borderbox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import { homeObjOne, homeObjTwo, homeObjThree } from './components/InfoSection/data';
+// import InfoSection from './components/InfoSection'
+// import Navbar from './components/Navbar'
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar/>
+//       <InfoSection {...homeObjOne} />
+
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import { homeObjOne, homeObjTwo, homeObjThree } from './components/InfoSection/data';
-import InfoSection from './components/InfoSection'
-import Navbar from './components/Navbar'
+import Home from './pages'
+// import SigninPage from './pages/signin';
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <InfoSection {...homeObjOne} />
-
+    <Router >
+      <Switch>
+      <Route path="/" component={Home} exact />
+      {/* <Route path="/signin" component={SigninPage} exact /> */}
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React , {useState} from 'react';
+// import Sidebar from '../components/sidebar';
+import Navbar from '../components/Navbar';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/data';
+import Footer from '../components/Footer';
+
+
+const Home = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <> 
+
+        {/* <Sidebar isOpen={isOpen} toggle={toggle}></Sidebar> */}
+        <Navbar toggle={toggle} />
+        <InfoSection {...homeObjOne} />
+        {/* <InfoSection {...homeObjTwo} />
+        <Services></Services>
+        <InfoSection {...homeObjThree} /> */}
+        <Footer/>
+        </>
+    )
+}
+
+export default Home
