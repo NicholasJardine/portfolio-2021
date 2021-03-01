e8c36a8321f8dd62a58a7fc343056dbfbf31071c
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-03-01 14:31:23 +0200

    first info section in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import { homeObjOne, homeObjTwo, homeObjThree } from './components/InfoSection/data';
+import InfoSection from './components/InfoSection'
 import Navbar from './components/Navbar'
 function App() {
   return (
     <Router>
       <Navbar/>
+      <InfoSection {...homeObjOne} />
+
     </Router>
   );
 }
