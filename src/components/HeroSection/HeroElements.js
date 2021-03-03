8c58dca91e370278a050dacce73dd8a13c713a85
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-03-03 11:01:24 +0200

    button left

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -88,8 +88,7 @@ export const HeroP = styled.p`
 export const HeroBtnWrapper = styled.div`
     margin-top:32px;
     display:flex;
-    flex-direction:column;
-    align-items:center;
+    align-items:left;
 `
 
 export const ArrowForward = styled(MdArrowForward)`
